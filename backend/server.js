078e04a6bd635ac64e25df5b9e2da3ce804e2c15
Paw-wade <wadepaw@gmail.com>
2025-12-17 14:37:20 +0100

    fix eslint peer deps for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,10 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
